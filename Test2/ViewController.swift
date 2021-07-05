2140f5a539e9fc48ed63d0e2c30b4aba4124debe
COMDORI <02145s1@gmail.com>
2021-07-06 04:32:20 +0900

    Initial Commit

---
+++ Test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test2
+//
+//  Created by WONJI HA on 2021/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
