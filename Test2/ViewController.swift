b69fe76c841be211e8752251c3f8dad229115f75
COMDORI <02145s1@gmail.com>
2021-08-25 06:11:13 +0900

    1.1.1
    1. 프로젝트 이름 변경 Timer
    2. UI변경
    3. 타이머 다운시 0이하 음수로 안내려감

--- Test2/ViewController.swift
+++ Test2/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var t1  = 0;
 
+    
     @IBOutlet weak var TimerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +22,39 @@ class ViewController: UIViewController {
     @IBAction func B1(_ sender: Any) {
                 print("버튼을 눌렀습니다.")
     }
+
+    @IBAction func B2 (_ sender: Any) {
+        t1 += 1
+        print(t1,"값 입니다")
+        TimerLabel.text="\(t1)"
+        
+    }
+    
+    @IBAction func tapLongPress(_ sender: UILongPressGestureRecognizer){
+        if(sender.state == UIGestureRecognizer.State.began){
+            t1 += 1
+            TimerLabel.text="\(t1)"
+        }
+        else if (sender.state == UIGestureRecognizer.State.ended){
+            print("길게 터치 중지")
+        }
+    }
+    
+    @IBAction func B3 (_ sender: Any) {
+        if(t1>1){
+            t1 = t1-1
+        }
+        else if(t1>0){
+            t1 -= 1
+        }
+        
+        print(t1,"값 입니다")
+        TimerLabel.text="\(t1)"
+
+        
+    }
+    
+    
     func onTapButton() {
         
         
