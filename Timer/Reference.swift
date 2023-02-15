00010b2983170348e4f6366af8e9545ff069bf2e
comdori <02145s1@gmail.com>
2023-02-16 03:48:40 +0900

    Create Reference.swift
    참고 및 공부

---
+++ Timer/Reference.swift
@@ -0,0 +1,24 @@
+//
+//  Reference.swift
+//  Timer
+//
+//  Created by Wonji Ha on 2023/02/16.
+//
+/**참고 및 공부**/
+/*
+ https://es1015.tistory.com/504 [Swift] FileManager로 디바이스에 파일 저장하기 (Document) [LSSupportsOpeningDocumentsInPlace entry or an UISupportsDocumentBrowser entry to your Info.plist to declare support.]
+
+ https://inuplace.tistory.com/1163 로컬라이징
+ 
+ https://velog.io/@brillantescene/%EC%8A%A4%EC%9C%84%ED%94%84%ED%8A%B8-%ED%8C%8C%EC%9D%BC-%EC%A0%95%EB%A6%AC 프로젝트 구조 정리1
+ 
+ https://mini-min-dev.tistory.com/15 프로젝트 구조 정리2(프로젝트 폴더링)
+ 
+ https://www.clien.net/service/board/cm_app/17167370 클리앙 개발 문의
+옵셔널 체이닝: 변수나 상수 뒤에 ? 또는 !느낌표를 사용하여 옵셔널에서 값을 강제 추출하는 효과가 있다. 사용을 지양하는 편이 좋다고 한다.
+ 
+ https://fomaios.tistory.com/entry/Swift-Enum%EC%97%B4%EA%B1%B0%ED%98%95%EC%9D%84-%EC%8D%A8%EC%95%BC-%ED%95%98%EB%8A%94-%EC%9D%B4%EC%9C%A0 Enum 열거형 이란?
+ 
+ https://pskbhnsr.tistory.com/48 딕셔너리 정렬
+ 
+*/
