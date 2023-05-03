2145b18f366868c3aea60dd52e32b88864bc2821
comdori <02145s1@gmail.com>
2023-05-03 21:37:30 +0900

    Feat 셀 선택후 바로 선택해제

--- Timer/Sources/Cells/SettingTableCell.swift
+++ Timer/Sources/Cells/SettingTableCell.swift
@@ -8,6 +8,7 @@
 /* Reference
  https://philosopher-chan.tistory.com/1032 테이블 셀 클릭 이벤트 처리
  https://stackoverflow.com/questions/37558333/select-cell-in-tableview-section 테이블 섹션 구분
+ https://sweetdev.tistory.com/105 셀 선택시 바로 deselect 시켜버리기
  https://velog.io/@minji0801/iOS-Swift-iOS-%EA%B8%B0%EA%B8%B0%EC%97%90%EC%84%9C-Mail-%EC%95%B1-%EC%9D%B4%EC%9A%A9%ED%95%B4%EC%84%9C-%EC%9D%B4%EB%A9%94%EC%9D%BC-%EB%B3%B4%EB%82%B4%EB%8A%94-%EB%B0%A9%EB%B2%95 메일 보내기
  https://borabong.tistory.com/6 메일컨트롤러 dismiss
  */
@@ -29,6 +30,9 @@ class SettingTableCell:UITableViewController, MFMailComposeViewControllerDelegat
         func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
             self.dismiss(animated: true, completion: nil)
         }
+        guard let info = storyboard?.instantiateViewController(identifier: "Info") as? Info else { return }
+        let navigationController = UINavigationController(rootViewController: info)
+        present(navigationController, animated: true)
     }
 
     @IBOutlet var SoundSwitch: UISwitch!
@@ -96,12 +100,30 @@ class SettingTableCell:UITableViewController, MFMailComposeViewControllerDelegat
         
         return identifier
     }
+    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell()
+//        cell.selectionStyle = .none
+//    }
+    
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("테이블셀을 클릭 했습니다")
-        print(indexPath.section,"섹션의", indexPath.row , "행입니다.")
-        
+        print("테이블셀을 클릭 했습니다", indexPath.section,"섹션의", indexPath.row , "행입니다.")
+        tableView.deselectRow(at: indexPath, animated: true) // 셀 선택후 바로 선택해제 하기
+        if(indexPath.section == 1 && indexPath.row == 0){
+//           let infoVC = self.storyboard?.instantiateViewController(withIdentifier: "Info")
+//            self.navigationController?.pushViewController(infoVC!, animated: true)
+            
+//            let vcName = self.storyboard?.instantiateViewController(withIdentifier: "Info")
+//                    vcName?.modalPresentationStyle = .fullScreen //전체화면으로 보이게 설정
+//                    vcName?.modalTransitionStyle = .crossDissolve //전환 애니메이션 설정
+//                    self.present(vcName!, animated: true, completion: nil)
+            
+            let viewController = self.storyboard?.instantiateViewController(withIdentifier: "Info")
+                    self.navigationController?.pushViewController(viewController!, animated: true)
+        }
         if(indexPath.section == 1 && indexPath.row == 2){
+            
             if MFMailComposeViewController.canSendMail() {
                 let composeViewController = MFMailComposeViewController()
                 composeViewController.mailComposeDelegate = self
@@ -109,7 +131,7 @@ class SettingTableCell:UITableViewController, MFMailComposeViewControllerDelegat
                 let msgBody =
                 """
                 아이폰 모델 : \(self.getDeviceIdentifier())
-                iOS : \(UIDevice.current.systemVersion)
+                iOS 버전 : \(UIDevice.current.systemVersion)
                 앱 버전 : \(version())
                 ┈┈┈┈┈┈┈✄┈┈┈┈┈┈┈
                 
@@ -128,8 +150,8 @@ class SettingTableCell:UITableViewController, MFMailComposeViewControllerDelegat
 
             } else {
                 print("send Mail Fail")
-                let sendMailErrorAlert = UIAlertController(title: "메일 보내기 오류", message: "현재 메일 앱이 설치되어 있지 않거나 이메일앱 설정이 되어 있지 않습니다. App Store에서 Apple Mail앱 다운로드 또는 이메일 설정을 확인해 주시기를 바랍니다.", preferredStyle: .alert)
-                let mailInstall = UIAlertAction(title: "다운로드", style: .default) { _ in
+                let sendMailErrorAlert = UIAlertController(title: "문의사항 보내기 오류", message: "현재 메일 앱이 설치되어 있지 않거나 이메일앱 설정이 되어 있지 않습니다. App Store에서 Apple Mail앱 다운로드 또는 이메일 설정을 확인해 주시기를 바랍니다.", preferredStyle: .alert)
+                let mailInstall = UIAlertAction(title: "메일앱 다운로드", style: .default) { _ in
                     if let url = URL(string: "https://apps.apple.com/kr/app/mail/id1108187098"), UIApplication.shared.canOpenURL(url) {
                         if #available(iOS 10.0, *) {
                             UIApplication.shared.open(url, options: [:], completionHandler: nil)
@@ -146,7 +168,7 @@ class SettingTableCell:UITableViewController, MFMailComposeViewControllerDelegat
         }
     }
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
-        self.dismiss(animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil) // 메일 보내기 창 dismiss
     }
 }
 
