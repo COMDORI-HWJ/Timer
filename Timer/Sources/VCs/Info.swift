7a0013ecd5efb39282996ebea3ee8be856c5c811
comdori <02145s1@gmail.com>
2023-05-04 06:00:53 +0900

    Update Info.swift
    - 뒤로 버튼 추가
    - 이 앱에 관하여 설명 수정

--- Timer/Sources/VCs/Info.swift
+++ Timer/Sources/VCs/Info.swift
@@ -16,9 +16,8 @@ class Info: UIViewController {
 
     @IBOutlet weak var Profile: UIImageView!
     @IBOutlet weak var Info: UITextView!
+    @IBOutlet weak var backBtn: UIBarButtonItem!
     
-    let info = "안녕하세요!\n앱 개발자 하원지입니다.\n밀리초타이머앱을 사용해주셔서 감사합니다.\n이 앱을 만들면서 많은 어려움을 겪었지만 출시하게 되어 정말 기쁘고 행복합니다. \n밀리초타이머 앱을 사용하면서 불편하거나 버그는 제보해주시면 감사합니다.\n또한 톡톡튀는 아이디어가 있으시다면 주저하지 마시고 문의 주시면 대단히 감사하겠습니다.\n추후 검토후 최대한 반영하여 업데이트 하겠습니다. \n\n마지막으로 앱 리뷰를 부탁드리겠습니다.\n짧아도 좋습니다! \n리뷰를 적어주시면 대단히 감사하겠습니다. \n좋은 하루 보내세요!"
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +25,10 @@ class Info: UIViewController {
         //Info.font = .systemFont(ofSize: 13.0)
         Info.isEditable = false
         // Do any additional setup after loading the view.
+        
     }
+    
+    let info = "안녕하세요!\n앱 개발자 하원지입니다.\n밀리초타이머앱을 사용해 주셔서 감사합니다.\n이 앱을 만들면서 많은 어려움을 겪었지만 출시하게 되어 정말 기쁘고 행복합니다. \n밀리초타이머 앱을 사용하면서 불편한 점이나 버그는 제보해 주시면 수정하도록 하겠습니다.\n또한 톡톡 튀는 아이디어가 있으시다면 주저하지 마시고 문의 하시면 대단히 감사하겠습니다.\n추후 검토 후 최대한 반영하여 업데이트하겠습니다. \n\n마지막으로 앱 리뷰를 부탁드리겠습니다.\n짧아도 좋습니다! \n감사합니다. \n좋은 하루 보내세요!"
     
     /*
     // MARK: - Navigation
@@ -38,4 +40,10 @@ class Info: UIViewController {
     }
     */
  
+    
+    @IBAction func backBtn(_ sender: Any) {
+//        self.presentedViewController?.dismiss(animated: true, completion: nil)
+        dismiss(animated: true, completion: nil)
+        print("뒤로가기 버튼을 눌렀습니다.")
+    }
 }
