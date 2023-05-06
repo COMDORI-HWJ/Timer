474f4bbd8047da17dd39f1e5b99c692cfaac65b9
comdori <02145s1@gmail.com>
2023-05-06 09:29:29 +0900

    Feat 튜토리얼 화면 추가

--- Timer/Sources/Classes/MainTabBar.swift
+++ Timer/Sources/Classes/MainTabBar.swift
@@ -8,18 +8,30 @@
 import UIKit
 
 class MainTabBar: UITabBarController {
+    
+//    let appDelegate = UIApplication.shared.delegate as! AppDelegate
 
     override func viewDidLoad() {
+
         super.viewDidLoad()
+        
         self.selectedIndex = 0 //첫 시작 화면을 탭바 0번으로 시작
         //                tabBarController?.selectedIndex = 3
-
-
+        
         // Do any additional setup after loading the view.
         NotificationCenter.default.addObserver(self, selector: #selector(showPage(_:)), name: NSNotification.Name("showPage"), object: nil) //푸시 알림 옵저버
     }
-
     
+    override func viewDidAppear(_ animated: Bool) { // https://scshim.tistory.com/284 whose view is not in the window hierarchy!
+        print("튜토리얼화면작동")
+        let ud = UserDefaults.standard
+        if ud.bool(forKey: UserInfoKey.tutorial) == true {
+            let vc = instanceTutorialVC(name: "MasterVC")
+            self.present(vc!, animated: true)
+            return
+        }
+    }
+
     @objc func showPage(_ notification:Notification) {
         if let userInfo = notification.userInfo {
             if let index = userInfo["index"] as? Int {
@@ -37,5 +49,14 @@ class MainTabBar: UITabBarController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+//    func checkFirstRun() {
+//        let ud = UserDefaults.standard
+//        if ud.bool(forKey: UserInfoKey.tutorial) == true {
+//            print("before ud=\(ud.bool(forKey: UserInfoKey.tutorial))")
+//            t.modalPresentationStyle = .automatic
+//            self.present(t!, animated: true)
+//        }
+//    }
+    
 }
