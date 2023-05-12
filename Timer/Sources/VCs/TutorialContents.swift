4dd2cdd490e0d97437e8926d23f18c9dc77c12e8
comdori <02145s1@gmail.com>
2023-05-13 05:46:45 +0900

    Feat 제목 위치 수정

--- Timer/Sources/VCs/TutorialContents.swift
+++ Timer/Sources/VCs/TutorialContents.swift
@@ -4,7 +4,11 @@
 //
 //  Created by Wonji Ha on 2023/05/06.
 //
-
+/** Reference
+ * https://kkh0977.tistory.com/2753 CGRect 사용해 x , y , width , height 좌표, 크기 설정 및 동적으로 UILabel 라벨 생성 실시
+ * https://woongsios.tistory.com/57 status bar 높이 구하기 in Swift 5
+ 
+ */
 import UIKit
 
 class TutorialContents: UIViewController {
@@ -14,37 +18,46 @@ class TutorialContents: UIViewController {
     var pageIndex: Int!
     
     var titleLabel: UILabel!
-    @IBOutlet weak var bgImageView: UIImageView!
-
+    var bgImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        titleLabel = UILabel()
-//        bgImageView = UIImageView()
+                
+        bgImageView = UIImageView()
         bgImageView.image = UIImage(named: imageFile)
-
+        titleLabel = UILabel()
+        titleLabel.text = titleText
+        titleLabel.sizeToFit() // 레이블 객체 텍스트 동적 설정 레이블 너비를 텍스트 길에 맞추는 역할
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.boldSystemFont(ofSize: UIFont.labelFontSize)
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     MARK: - Navigation
+     
+     In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     Get the new view controller using segue.destination.
+     Pass the selected object to the new view controller.
+     }
+     */
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        titleLabel.text = titleText
-//        print("현재 페이지: " , titleText!)
-//        guard let img = UIImage(named: imageFile) else { return}
-//        bgImageView.image = img
-//        bgImageView.frame = CGRect(x: 0, y: 0, width: view.safeAreaLayoutGuide.layoutFrame.width, height: view.safeAreaLayoutGuide.layoutFrame.height)
-//        view.addSubview(bgImageView)
-//        titleLabel.frame = CGRect(x: view.frame.width/2, y: view.frame.height-100, width: 100, height: 30)
-//        view.addSubview(titleLabel)
-//    }
-
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // 디바이스 화면 사이즈 확인
+        let deviceWidth = self.view.frame.size.width
+        
+        // 디바이스 상태 바 높이 사이즈 확인
+        let statusBarFrameHeight = view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+        
+        titleLabel.frame = CGRect(x: 0, y: statusBarFrameHeight/3, width: deviceWidth, height: 30)
+        print("현재 페이지: " , titleText!)
+        guard let img = UIImage(named: imageFile) else { return}
+        bgImageView.image = img
+        bgImageView.frame = CGRect(x: 0, y: 0, width: view.safeAreaLayoutGuide.layoutFrame.width, height: view.safeAreaLayoutGuide.layoutFrame.height)
+        view.addSubview(bgImageView)
+        view.addSubview(titleLabel)
+//        titleLabel.frame = CGRectInset(CGRect, CGFloat, CGFloat)
+    }
 }
