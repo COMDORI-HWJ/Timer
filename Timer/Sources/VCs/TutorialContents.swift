93bdf89f40814f09d00300150aa0d3f64a78c771
comdori <02145s1@gmail.com>
2023-05-28 05:25:27 +0900

    Feat 사용 방법 설명 기능 추가 외 레이아웃 변경
    - 오토레이아웃 설정
    - 사용 방법 설명 추가 작업

--- Timer/Sources/VCs/TutorialContents.swift
+++ Timer/Sources/VCs/TutorialContents.swift
@@ -16,12 +16,14 @@ import Gifu
 class TutorialContents: UIViewController {
     
     var titleText: String!
+    var contentText: String!
     var imageFile: String!
     var pageIndex: Int!
     
     var titleLabel: UILabel!
+    var contentLabel: UILabel!
     let gifImageView = GIFImageView()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,23 +33,40 @@ class TutorialContents: UIViewController {
         titleLabel.text = titleText
         titleLabel.sizeToFit() // 레이블 객체 텍스트 동적 설정 레이블 너비를 텍스트 길에 맞추는 역할
         titleLabel.textAlignment = .center
-        titleLabel.font = UIFont.boldSystemFont(ofSize: UIFont.labelFontSize)
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 30)
+        
+        contentLabel = UILabel()
+        contentLabel.text = contentText
+        contentLabel.sizeToFit()
+        contentLabel.numberOfLines = 0 // 자동 줄바꿈
+        contentLabel.textAlignment = .center
+        contentLabel.font = UIFont.systemFont(ofSize: UIFont.labelFontSize)
     }
         
     override func viewWillAppear(_ animated: Bool) {
         
         // 디바이스 화면 사이즈 확인
+        let bounds = UIScreen.main.bounds
+        let boundsHeight = bounds.size.height
         let deviceWidth = self.view.frame.size.width
+        let deviceHeight = self.view.frame.size.height
         
         // 디바이스 상태 바 높이 사이즈 확인
         let statusBarFrameHeight = view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
         
-        titleLabel.frame = CGRect(x: 0, y: statusBarFrameHeight/3, width: deviceWidth, height: 30)
+        titleLabel.frame = CGRect(x: 0, y: deviceHeight/20, width: deviceWidth, height: 30)
         print("현재 페이지: " , titleText!)
+        contentLabel.frame = CGRect(x: 0, y: deviceHeight/9, width: deviceWidth, height: 50)
         
-        gifImageView.frame = CGRect(x: 0, y: 0, width: view.safeAreaLayoutGuide.layoutFrame.width, height: view.safeAreaLayoutGuide.layoutFrame.height)
+        
+        gifImageView.frame = CGRect(x: 10, y: boundsHeight*0.2, width: deviceWidth-20, height: boundsHeight/2)
+        gifImageView.translatesAutoresizingMaskIntoConstraints = true // 오토레이아웃 사용시 AutoresizingMask를 사용한 Constraints 변환을 막는 것
+//        gifImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100).isActive = true
+//        gifImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 100).isActive = true
+//        gifImageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -80).isActive = true
         view.addSubview(gifImageView)
         view.addSubview(titleLabel)
+        view.addSubview(contentLabel)
 //        titleLabel.frame = CGRectInset(CGRect, CGFloat, CGFloat)
     }
 }
