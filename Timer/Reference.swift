c3483314d6ec4a501dc7ef88cef28aa19847d5a1
comdori <02145s1@gmail.com>
2023-05-29 06:20:49 +0900

    Docs 백그라운드 모드 사용 방법
    백그라운드 모드를 사용하여 백그라운드 상태에서도 타이머가 작동한다.

--- Timer/Reference.swift
+++ Timer/Reference.swift
@@ -5,37 +5,22 @@
 //  Created by Wonji Ha on 2023/02/16.
 //
 /**참고 및 공부**/
-/*
-https://noahlogs.tistory.com/9 [기초]변수와 상수
- https://es1015.tistory.com/504 [Swift] FileManager로 디바이스에 파일 저장하기 (Document) [LSSupportsOpeningDocumentsInPlace entry or an UISupportsDocumentBrowser entry to your Info.plist to declare support.]
+/**
+ *
+ *
+ * https://noahlogs.tistory.com/9 [기초]변수와 상수
+ * https://es1015.tistory.com/504 [Swift] FileManager로 디바이스에 파일 저장하기 (Document) [LSSupportsOpeningDocumentsInPlace entry or an UISupportsDocumentBrowser entry to your Info.plist to declare support.]
+ * https://inuplace.tistory.com/1163 로컬라이징
+ * https://velog.io/@brillantescene/%EC%8A%A4%EC%9C%84%ED%94%84%ED%8A%B8-%ED%8C%8C%EC%9D%BC-%EC%A0%95%EB%A6%AC 프로젝트 구조 정리1
 
- https://inuplace.tistory.com/1163 로컬라이징
- 
- https://velog.io/@brillantescene/%EC%8A%A4%EC%9C%84%ED%94%84%ED%8A%B8-%ED%8C%8C%EC%9D%BC-%EC%A0%95%EB%A6%AC 프로젝트 구조 정리1
- 
- https://mini-min-dev.tistory.com/15 프로젝트 구조 정리2(프로젝트 폴더링)
- 
- https://www.clien.net/service/board/cm_app/17167370 클리앙 개발 문의
-옵셔널 체이닝: 변수나 상수 뒤에 ? 또는 !느낌표를 사용하여 옵셔널에서 값을 강제 추출하는 효과가 있다. 사용을 지양하는 편이 좋다고 한다.
- 
- https://fomaios.tistory.com/entry/Swift-Enum%EC%97%B4%EA%B1%B0%ED%98%95%EC%9D%84-%EC%8D%A8%EC%95%BC-%ED%95%98%EB%8A%94-%EC%9D%B4%EC%9C%A0 Enum 열거형 이란?
- 
- https://pskbhnsr.tistory.com/48 딕셔너리 정렬
- 
- https://junyng.tistory.com/30 Dynamic Type을 사용하여 폰트 크기 조정하기
- 
+ * https://mini-min-dev.tistory.com/15 프로젝트 구조 정리2(프로젝트 폴더링)
+ * https://www.clien.net/service/board/cm_app/17167370 클리앙 개발 문의
+ 옵셔널 체이닝: 변수나 상수 뒤에 ? 또는 !느낌표를 사용하여 옵셔널에서 값을 강제 추출하는 효과가 있다. 사용을 지양하는 편이 좋다고 한다.
+ * https://fomaios.tistory.com/entry/Swift-Enum%EC%97%B4%EA%B1%B0%ED%98%95%EC%9D%84-%EC%8D%A8%EC%95%BC-%ED%95%98%EB%8A%94-%EC%9D%B4%EC%9C%A0 Enum 열거형 이란?
+ * https://pskbhnsr.tistory.com/48 딕셔너리 정렬
+ * https://junyng.tistory.com/30 Dynamic Type을 사용하여 폰트 크기 조정하기
+ * https://velog.io/@2004jmk/Background%EC%97%90%EC%84%9C-%EC%95%B1-%EA%B3%84%EC%86%8D%ED%95%B4%EC%84%9C-%EC%8B%A4%ED%96%89%ED%95%98%EA%B8%B0-Swift Background에서 앱 계속해서 실행하기
 */
 
 
 
-//
-//  TutorialContents.swift
-//  Timer
-//
-//  Created by Wonji Ha on 2023/05/06.
-//
-/** Reference
- * https://kkh0977.tistory.com/2753 CGRect 사용해 x , y , width , height 좌표, 크기 설정 및 동적으로 UILabel 라벨 생성 실시
- * https://woongsios.tistory.com/57 status bar 높이 구하기 in Swift 5
- 
- */
