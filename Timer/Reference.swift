32507f3c32dacb93460143155033eb2422651533
comdori <02145s1@gmail.com>
2023-05-31 05:41:03 +0900

    [DOCS] 포어그라운드 백그라운드 학습

--- Timer/Reference.swift
+++ Timer/Reference.swift
@@ -9,10 +9,10 @@
  *
  *
  * https://noahlogs.tistory.com/9 [기초]변수와 상수
+ * https://80000coding.oopy.io/0bd77cd3-7dc7-4cf4-93ee-8ca4fbca898e 가드문 사용법 (if문보다 빠르게 끝낸다)
  * https://es1015.tistory.com/504 [Swift] FileManager로 디바이스에 파일 저장하기 (Document) [LSSupportsOpeningDocumentsInPlace entry or an UISupportsDocumentBrowser entry to your Info.plist to declare support.]
  * https://inuplace.tistory.com/1163 로컬라이징
  * https://velog.io/@brillantescene/%EC%8A%A4%EC%9C%84%ED%94%84%ED%8A%B8-%ED%8C%8C%EC%9D%BC-%EC%A0%95%EB%A6%AC 프로젝트 구조 정리1
-
  * https://mini-min-dev.tistory.com/15 프로젝트 구조 정리2(프로젝트 폴더링)
  * https://www.clien.net/service/board/cm_app/17167370 클리앙 개발 문의
  옵셔널 체이닝: 변수나 상수 뒤에 ? 또는 !느낌표를 사용하여 옵셔널에서 값을 강제 추출하는 효과가 있다. 사용을 지양하는 편이 좋다고 한다.
@@ -20,6 +20,8 @@
  * https://pskbhnsr.tistory.com/48 딕셔너리 정렬
  * https://junyng.tistory.com/30 Dynamic Type을 사용하여 폰트 크기 조정하기
  * https://velog.io/@2004jmk/Background%EC%97%90%EC%84%9C-%EC%95%B1-%EA%B3%84%EC%86%8D%ED%95%B4%EC%84%9C-%EC%8B%A4%ED%96%89%ED%95%98%EA%B8%B0-Swift Background에서 앱 계속해서 실행하기
+ * https://icksw.tistory.com/178 Foreground, Background 알아보기
+ * 
 */
 
 
