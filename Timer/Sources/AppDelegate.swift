45137c279e14261aa96c51b9a714620c4ee6304b
comdori <02145s1@gmail.com>
2023-06-27 07:24:58 +0900

    Add requestNotificationPermission
    푸시 알림 기능 추가

--- Timer/Sources/AppDelegate.swift
+++ Timer/Sources/AppDelegate.swift
@@ -16,11 +16,15 @@ import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    
     var window: UIWindow?
-    let userNotifiNotificationCenter = UNUserNotificationCenter.current()
-
+    //    let notiContent = UNMutableNotificationContent()
+    let notiCenter = UNUserNotificationCenter.current()
+    
+    @Published var isAlertOccurred: Bool = false
+    
+    
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         print("이제 앱 실행 준비할게요")
         return true
@@ -33,40 +37,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         GADMobileAds.sharedInstance().start(completionHandler: nil)
         
-//        UNUserNotificationCenter.current().delegate = self
+        notiCenter.delegate = self // 특정 ViewController에 구현되어 있으면 푸시를 받지 못할 가능성이 있으므로 AppDelegate에서 구현(앱에서 포그라운드 푸시 알림)
+        requestNotiAuthorization()
         
-        userNotifiNotificationCenter.delegate = self // 특정 ViewController에 구현되어 있으면 푸시를 받지 못할 가능성이 있으므로 AppDelegate에서 구현(앱에서 푸시알림)
-
-//        userNotifiNotificationCenter.requestAuthorization(
-//            options: [.alert, .sound, .badge], completionHandler: { ( granted, error) in
-//                print("granted notification, \(granted)")
-//            })
+        //        requestNotificationPermission()
         
-        application.registerForRemoteNotifications()
+        //        UNUserNotificationCenter.current().delegate = self
+        
+        
+        
+        //        application.registerForRemoteNotifications()
+        //        UIApplication.shared.registerForRemoteNotifications()
         
         return true
     }
     
     /*
-          앱이 종료되기 직전에 호출된다.
-       하지만 메모리 확보를 위해 suspended 상태에 있는 앱이 종료될 때나
-       background 상태에서 사용자에 의해 종료될 때나
-       오류로 인해 앱이 종료될 때는 호출되지 않는다.
-         */
+     앱이 종료되기 직전에 호출된다.
+     하지만 메모리 확보를 위해 suspended 상태에 있는 앱이 종료될 때나
+     background 상태에서 사용자에 의해 종료될 때나
+     오류로 인해 앱이 종료될 때는 호출되지 않는다.
+     */
     
-      func applicationWillTerminate(_ application: UIApplication) {
-              print("이제 곧 종료될거에요")
-          }
-
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("이제 곧 종료될거에요")
+    }
+    
     
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
@@ -74,9 +79,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-           
-           return UIInterfaceOrientationMask.portrait // 세로 화면 고정
-       }
+        
+        return UIInterfaceOrientationMask.portrait // 세로 화면 고정
+    }
+    
+    func requestNotiAuthorization() // 노티피케이션 최초 허락
+    {
+        notiCenter.getNotificationSettings { settings in
+            
+            // 승인되어있지 않은 경우 request
+            if settings.authorizationStatus != .authorized {
+                self.notiCenter.requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { success, error in
+                    
+                    // 거부되어있는 경우 alert
+                    if settings.authorizationStatus == .denied {
+                        // 알림 띄운 뒤 설정 창으로 이동
+                        DispatchQueue.main.async {
+                            self.isAlertOccurred = true
+                        }
+                    }
+                    
+                    if success
+                    {
+                        print("Push 권한 허용")
+                    }
+                    else
+                    {
+                        print("Push 권한 거부")
+                    }
+                    
+                    if let error = error {
+                        print("Error : \(error)")
+                    }
+                    
+                    
+                })
+            }
+        }
+    }
 }
     
 extension AppDelegate : UNUserNotificationCenterDelegate {
@@ -84,7 +124,7 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
     // ForeGround 에서 작동
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         
-//        let _ = notification.request.content.userInfo // deep link 처리 시 아래 _ 값 가지고 처리
+        let _ = notification.request.content.userInfo // deep link 처리 시 아래 _ 값 가지고 처리
         //
         
         //        completionHandler()
@@ -94,13 +134,13 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
     }
     
     // Background 에서 작동
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    private func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         
         
         let _ = response.notification.request.content.userInfo // deep link 처리 시 아래 _ 값 가지고 처리
 
-//        completionHandler([.list, .badge, .sound, .banner])
-        completionHandler()
+        completionHandler([.list, .badge, .sound, .banner])
+//        completionHandler()
         
         let application = UIApplication.shared
         if application.applicationState == .active
