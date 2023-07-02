9365bd88a6181d1582a876527b672e236cd445f3
comdori <02145s1@gmail.com>
2023-07-03 06:21:59 +0900

    Fix 사용방법 최초 1회 실행

--- Timer/Sources/Classes/MainTabBar.swift
+++ Timer/Sources/Classes/MainTabBar.swift
@@ -25,9 +25,10 @@ class MainTabBar: UITabBarController {
     override func viewDidAppear(_ animated: Bool) { // https://scshim.tistory.com/284 whose view is not in the window hierarchy!
         print("튜토리얼 화면 작동")
         let ud = UserDefaults.standard
-        if ud.bool(forKey: UserInfoKey.tutorial) == true {
+        if ud.bool(forKey: UserInfoKey.tutorial) == false {
             let vc = instanceTutorial(name: "TutorialVC")
             self.present(vc!, animated: true)
+            
             return
         }
     }
