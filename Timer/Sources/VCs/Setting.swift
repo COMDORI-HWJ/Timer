5773bf669c5b9c3ef6e2cf9c43476870b7128b52
comdori <02145s1@gmail.com>
2023-07-06 17:12:52 +0900

    Feat Admob
    설정 화면 광고 추가

--- Timer/Sources/VCs/Setting.swift
+++ Timer/Sources/VCs/Setting.swift
@@ -7,12 +7,20 @@
 
 import Foundation
 import UIKit
+import GoogleMobileAds
 
 class Setting: UIViewController { // UITableViewDataSource, UITableViewDelegate
     
     @IBOutlet weak var Table: UITableView!
+    @IBOutlet weak var bannerView: GADBannerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        /* Admob */
+     //  bannerView.adUnitID = "ca-app-pub-7875242624363574/7192134359"
+       bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716" // 테스트 광고
+       bannerView.rootViewController = self
+       bannerView.load(GADRequest())
     }
 }
