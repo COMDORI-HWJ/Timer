04417063a61940bf319c6648fe757aafa95d613f
comdori <02145s1@gmail.com>
2023-07-11 00:20:20 +0900

    Feat 앱 버전 추가 및 앱 아이콘 추가
    - 설정 화면에서 앱버전을 이 앱에 관하여로 옮김
    - 컴돌이 로고에서 앱 아이콘으로 변경

--- Timer/Sources/VCs/Info.swift
+++ Timer/Sources/VCs/Info.swift
@@ -18,17 +18,21 @@ class Info: UIViewController {
     @IBOutlet weak var Info: UITextView!
     @IBOutlet weak var backBtn: UIBarButtonItem!
     
+    @IBOutlet weak var ver: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         Info.text = info
         //Info.font = .systemFont(ofSize: 13.0)
         Info.isEditable = false
         // Do any additional setup after loading the view.
+        appver()
         
     }
     
-    let info = "안녕하세요!\n앱 개발자 하원지입니다.\n밀리초타이머앱을 사용해 주셔서 감사합니다.\n이 앱을 만들면서 어려움을 겪었지만 출시하게 되어 정말 기쁘고 행복합니다. \n밀리초타이머 앱을 사용하면서 불편한 점이나 버그는 제보해 주시면 수정하도록 하겠습니다.\n또한 새로운 기능 아이디어가 있으시다면 주저하지 마시고 의견을 주시면 추후 검토 후 최대한 반영하여 업데이트하겠습니다. \n\n마지막으로 앱 리뷰를 부탁드리겠습니다.\n짧아도 좋습니다! \n좋은 하루 보내세요! \n감사합니다."
+    let info = "안녕하세요! 🙋🏻‍♂️\n앱 개발자 하원지입니다.\n밀리초타이머 앱을 사용해 주셔서 감사합니다.\n이 앱을 만들면서 어려움을 겪었지만 출시하게 되어 정말 기쁘고 행복합니다. \n밀리초타이머 앱을 사용하면서 불편한 점이나 버그는 제보해 주시면 수정하도록 하겠습니다.\n또한 새로운 기능 아이디어가 있으시다면 주저하지 마시고 의견을 주시면 추후 검토 후 최대한 반영하여 업데이트하겠습니다. \n\n마지막으로 앱 리뷰를 부탁드리겠습니다.\n짧아도 좋습니다! \n좋은 하루 보내세요! \n감사합니다."
     
     /*
     // MARK: - Navigation
@@ -46,4 +50,11 @@ class Info: UIViewController {
         dismiss(animated: true, completion: nil)
         print("뒤로가기 버튼을 눌렀습니다.")
     }
+    
+    func appver ()
+    {
+        print(SettingTableCell().version())
+        ver.text =  String(format: NSLocalizedString("앱 버전 : ", comment: "App Version"))+"\(SettingTableCell().version())"
+    }
+    
 }
