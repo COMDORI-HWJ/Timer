e5973c111541ffd18194c454a5b22c018289ba1d
comdori <02145s1@gmail.com>
2023-07-19 00:17:45 +0900

    Remove 소스 정리

--- Timer/Sources/Classes/MainTabBar.swift
+++ Timer/Sources/Classes/MainTabBar.swift
@@ -9,17 +9,13 @@ import UIKit
 
 class MainTabBar: UITabBarController {
     
-//    let appDelegate = UIApplication.shared.delegate as! AppDelegate
-
+    
     override func viewDidLoad() {
-
+        
         super.viewDidLoad()
         
         self.selectedIndex = 0 // 첫 시작 화면을 탭바 0번으로 시작
-        //                tabBarController?.selectedIndex = 3
-        
-        // Do any additional setup after loading the view.
-        NotificationCenter.default.addObserver(self, selector: #selector(showPage(_:)), name: NSNotification.Name("showPage"), object: nil) //푸시 알림 옵저버
+        NotificationCenter.default.addObserver(self, selector: #selector(showPage(_:)), name: NSNotification.Name("showPage"), object: nil) // 푸시 알림 옵저버
     }
     
     override func viewDidAppear(_ animated: Bool) { // https://scshim.tistory.com/284 whose view is not in the window hierarchy!
@@ -28,11 +24,10 @@ class MainTabBar: UITabBarController {
         if ud.bool(forKey: UserInfoKey.tutorial) == false {
             let vc = instanceTutorial(name: "TutorialVC")
             self.present(vc!, animated: true)
-            
             return
         }
     }
-
+    
     @objc func showPage(_ notification:Notification) {
         if let userInfo = notification.userInfo {
             if let index = userInfo["index"] as? Int {
@@ -40,24 +35,4 @@ class MainTabBar: UITabBarController {
             }
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-//    func checkFirstRun() {
-//        let ud = UserDefaults.standard
-//        if ud.bool(forKey: UserInfoKey.tutorial) == true {
-//            print("before ud=\(ud.bool(forKey: UserInfoKey.tutorial))")
-//            t.modalPresentationStyle = .automatic
-//            self.present(t!, animated: true)
-//        }
-//    }
-    
 }
