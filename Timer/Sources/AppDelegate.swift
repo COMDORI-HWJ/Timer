8035d14ebe0ee515010926a396d6a4bab6d3c314
comdori <02145s1@gmail.com>
2023-08-01 03:12:41 +0900

    Feat 앱 추적 권한 요청
    광고 개인 최적화 광고 제공을 위한 요청

--- Timer/Sources/AppDelegate.swift
+++ Timer/Sources/AppDelegate.swift
@@ -8,11 +8,14 @@
  
  https://fomaios.tistory.com/entry/iOS-%ED%91%B8%EC%89%AC-%EC%95%8C%EB%A6%BC-%ED%83%AD%ED%96%88%EC%9D%84-%EB%95%8C-%ED%8A%B9%EC%A0%95-%ED%8E%98%EC%9D%B4%EC%A7%80%EB%A1%9C-%EC%9D%B4%EB%8F%99%ED%95%98%EA%B8%B0 푸시알림 탭 특정뷰 이동(원래 뷰에서)
  https://velog.io/@yoonjong/Swift-Push-Notification-%EB%88%84%EB%A5%BC-%EB%95%8C-%ED%8A%B9%EC%A0%95-ViewController-%EB%9C%A8%EA%B2%8C-%ED%95%98%EA%B8%B0 푸시알림 특정뷰 이동(새뷰에서)
+ https://velog.io/@minji0801/iOS-Swift-%EC%95%B1-%EC%B6%94%EC%A0%81-%EA%B6%8C%ED%95%9C-Alert-%EB%9D%84%EC%9A%B0%EA%B8%B0 앱 추적 권한 요청
  */
 
 import UIKit
-import GoogleMobileAds
 import UserNotifications
+import AdSupport
+import AppTrackingTransparency
+import GoogleMobileAds
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -36,13 +39,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         GADMobileAds.sharedInstance().start(completionHandler: nil)
         
         requestNotificationAuthorization() // 최초 푸시 알림 권한 요청
+        setAppTracking()
         
         notiCenter.delegate = self // 특정 ViewController에 구현되어 있으면 푸시를 받지 못할 가능성이 있으므로 AppDelegate에서 구현(앱에서 포그라운드 푸시 알림)
         
         //        application.registerForRemoteNotifications()
         //        UIApplication.shared.registerForRemoteNotifications()
-        
-        
         
         return true
     }
@@ -111,6 +113,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
+    
+    func setAppTracking(){
+        NotificationHandler().askNotificationPermission {
+            // 다른 권한 요청 창보다 늦게 띄우기
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                if #available(iOS 14, *) {
+                    ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+                        switch status {
+                        case .authorized:        // 허용됨
+                            print("Authorized")
+                            print("IDFA = \(ASIdentifierManager.shared().advertisingIdentifier)")    // IDFA 접근
+                        case .denied:        // 거부됨
+                            print("Denied")
+                        case .notDetermined:    // 결정되지 않음
+                            print("Not Determined")
+                        case .restricted:        // 제한됨
+                            print("Restricted")
+                        @unknown default:        // 알려지지 않음
+                            print("Unknown")
+                        }
+                    })
+                }
+            }
+        }
+    }
+    
 }
     
 extension AppDelegate : UNUserNotificationCenterDelegate {
@@ -122,7 +150,7 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
         //
         
         //        completionHandler()
-        completionHandler([.list, .badge, .banner, .sound])
+        completionHandler([.list, .banner, .sound])
         
     }
     
@@ -148,6 +176,4 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
                         NotificationCenter.default.post(name: Notification.Name("showPage"), object: nil, userInfo: ["index": 0])
                         }
     }
-    
-    
 }
