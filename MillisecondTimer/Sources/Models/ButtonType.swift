998252f6d9279ba4f6ef5395cad3ee865fc1d2c9
comdori <02145s1@gmail.com>
2024-05-23 06:57:17 +0900

    Feat 버튼 타입 생성
    - 타이머와 스톱워치에서 공통으로 사용하는 버튼 타입 생성

---
+++ MillisecondTimer/Sources/Models/ButtonType.swift
@@ -0,0 +1,28 @@
+//
+//  ButtonType.swift
+//  MillisecondTimer
+//
+//  Created by Wonji Ha on 2024/05/23.
+//
+
+import Foundation
+
+enum ButtonType {
+    case start
+    case pause
+    case reset
+    case rap
+    
+    var description: String {
+        switch self {
+        case .start:
+            return String(format: NSLocalizedString("시작", comment: "Start"))
+        case .pause:
+            return String(format: NSLocalizedString("일시중지", comment: "Pause"))
+        case .reset:
+            return String(format: NSLocalizedString("초기화", comment: "Reset"))
+        case .rap:
+            return String(format: NSLocalizedString("기록", comment: "Rap"))
+        }
+    }
+}
