56bdf1e5ab1376fc6a3ccb56375505797b11c961
comdori-wj <02145s1@gmail.com>
2024-11-25 16:10:38 +0900

    Refactor 타이머 모델 유닛 추가

--- MillisecondTimer/Sources/Models/Mtimer.swift
+++ MillisecondTimer/Sources/Models/Mtimer.swift
@@ -15,3 +15,36 @@ struct Mtimer {
     var elapsed : Double = 0// 경과시간
     var backgroudTime : Date? // 백그라운드 경과시간
 }
+
+enum TimeUnit: String {
+    case hour
+    case minute
+    case second
+    case millisecond
+    
+    var description: String {
+        switch self {
+        case .hour:
+            return "시간"
+        case .minute:
+            return "분"
+        case .second:
+            return "초"
+        case .millisecond:
+            return "밀리초"
+        }
+    }
+    
+    func toSeconds(_ value: Double) -> Double {
+        switch self {
+        case .hour:
+            return value * 3600
+        case .minute:
+            return value * 60
+        case .second:
+            return value
+        case .millisecond:
+            return value * 0.001
+        }
+    }
+}
